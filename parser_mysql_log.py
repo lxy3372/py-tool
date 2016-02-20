a7c4fd2a71fa08754b04ff156c1f7ab675da7444
Riky <me@rikyliu.com>
2016-02-20 11:24:29 +0800

    fixed bug about send mail by nickname

--- parser_mysql_log.py
+++ parser_mysql_log.py
@@ -11,6 +11,7 @@ SELECT * FROM test.t_test_user;
 """
 
 from email.mime.text import MIMEText
+from email.header import Header
 import smtplib
 import time
 import os
@@ -82,11 +83,15 @@ def parser_log(log_file, start_line):
 
 def send_mail(sqls):
     sender = 'account@my.org'
+    sender_nickname = 'Riky'
     receiver = ['riky@my.org']
     subject = 'Mysql 慢查询日志'
     smtpserver = 'smtp.my.com'
     username = 'account@my.org'
     password = '****'
+
+    send_info = Header(sender_nickname, 'utf-8')
+    send_info.append("<"+sender+">", 'ascii')
     content = "<br><br>" + subject + time.strftime("%Y-%m-%d") + "<br><br>" \
               "<div style='width:100%;'><table cellpadding='0' cellspacing='0' width='100%' style='border:1px solid #000;'><tr>" \
               "<th style='border:1px solid #ccc;text-align:center;'>SQL</th>" \
@@ -106,7 +111,7 @@ def send_mail(sqls):
 
     msg = MIMEText(content, 'html', 'utf-8')  # 中文需参数‘utf-8'，单字节字符不需要
     msg['Subject'] = subject
-    msg['From'] = sender
+    msg['From'] = send_info
     msg['To'] = ";".join(receiver)
 
     smtp = smtplib.SMTP()
